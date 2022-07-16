db33573799a45c3245fa5d9893abcb5c573855fc
unbazinga <kizz4224@yahoo.com>
2022-07-16 08:59:18 +0000

    Create projects.svelte

---
+++ src/routes/projects.svelte
@@ -0,0 +1,33 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<section>
+	<h1>
+
+		to your new<br />Bruh App
+	</h1>
+
+	<h2>
+		try editing <strong> doing nothing </strong>
+	</h2>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+</style>
